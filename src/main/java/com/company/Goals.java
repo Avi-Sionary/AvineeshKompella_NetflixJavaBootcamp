efb621fe1bae48f6ba972d449a3c5109688a2838
Avineesh Kompella <AvineeshKom@gmail.com>
2022-01-25 15:28:20 -0800

    the class

---
+++ src/main/java/com/company/Goals.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Goals {
+
+    public static void main(String[] args) {
+        System.out.println("Learn Java");
+        System.out.println("Learn how to code efficiently");
+        System.out.println("Make new connections and learn from my mentor");
+    }
+}
