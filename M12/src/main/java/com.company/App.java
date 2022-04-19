a77d35f5130035d7becae34039b261dd60d06805
Avineesh Kompella <AvineeshKom@gmail.com>
2022-04-19 00:06:11 -0700

    Module 12 challenge

--- M12/src/main/java/com.company/App.java
+++ M12/src/main/java/com.company/App.java
@@ -1,36 +1,164 @@
 package com.company;
+import java.util.List;
+import java.util.ArrayList;
 
 public class App {
 
-    public static void addState(ArrayList<State> stateList, State s) {
-        stateList.add(s);
-    }
+    public static void addState(List<State> stateList, State s) { stateList.add(s); }
 
-    public static void deleteState(ArrayList<State> stateList, State s) {
+    public static void deleteState(List<State> stateList, State s) {
         stateList.remove(s);
     }
 
-    public static void searchStateByName(ArrayList<State> stateList, String name) {
+    public static void listStates(List<State> stateList) {
+
+        for (int s = 0; s <= stateList.size() - 1; s++) {
+            System.out.println(stateList.get(s).getName());
+        }
 
     }
 
-    public static void searchLessThanPop(ArrayList<State> stateList, int pop) {
+    public static void addCityToState(State s, City c) { s.addCity(c); }
+
+    public static void deleteCityFromState(State s, String c) {
+
+        for (City city: s.getCityList()) {
+
+            if (city.getName() == c) {
+                s.deleteCity(city);
+                break;
+            }
+        }
+    }
+
+    public static void listCities(State s) {
+
+        List<City> cities = s.getCityList();
+
+        for (int c = 0; c <= cities.size() - 1; c++) {
+            System.out.println(cities.get(c).getName());
+        }
 
     }
 
-    public static void searchMoreThanPop(ArrayList<State> stateList, int pop) {
+    public static State searchStateByName(List<State> stateList, String name) {
+
+        for (State s: stateList) {
+
+            if (s.getName() == name) {
+                System.out.println("Found: " + s.getName());
+                return s;
+            }
+
+        }
+
+        System.out.println("Error: State not found");
+        return null;
+    }
+
+    public static List<State> searchLessThanPop(List<State> stateList, int pop) {
+
+        List<State> states = new ArrayList<>();
+
+        for (State s: stateList) {
+
+            int total = 0;
+
+            for (City c: s.getCityList()) {
+                total += c.getPopulation();
+            }
+
+            if (total < pop) {
+                states.add(s);
+            }
+
+        }
+
+        return states;
 
     }
 
-    public static void searchCityInState(State s, String name) {
+    public static List<State> searchMoreThanPop(List<State> stateList, int pop) {
+
+        List<State> states = new ArrayList<>();
+
+        for (State s: stateList) {
+
+            int total = 0;
+
+            for (City c: s.getCityList()) {
+                total += c.getPopulation();
+            }
+
+            if (total >= pop) {
+                states.add(s);
+            }
+
+        }
+
+        return states;
 
     }
 
-    public static void searchLessThanPopForState(State s, int pop) {
+    public static City searchCityInState(State s, String name) {
+
+        for (City c: s.getCityList()) {
+
+            if (c.getName() == name) {
+                System.out.println("Found in " + s.getName() + ": " + c.getName());
+                return c;
+            }
+
+        }
+
+        System.out.println("Error: City not found in " + s.getName());
+        return null;
+    }
+
+    public static City getCapital(State s) {
+
+        for (City c: s.getCityList()) {
+
+            if (c.isCapital()) {
+                System.out.println("The capital of " + s.getName() + " is " + c.getName());
+                return c;
+            }
+
+        }
+
+        System.out.println("Error: Capital for the state of " + s.getName() + " is not in the database.");
+        return null;
+    }
+
+    public static List<City> searchLessThanPopForCity(State s, int pop) {
+
+        List<City> cities = new ArrayList<>();
+
+        for (City c: s.getCityList()) {
+
+            if (c.getPopulation() < pop) {
+                cities.add(c);
+            }
+
+        }
+
+        return cities;
 
     }
 
-    public static void searchMoreThanPopForState(State s, int pop) {
+    public static List<City> searchMoreThanPopForCity(State s, int pop) {
+
+        List<City> cities = new ArrayList<>();
+
+        for (City c: s.getCityList()) {
+
+            if (c.getPopulation() >= pop) {
+                cities.add(c);
+            }
+
+        }
+
+        return cities;
 
     }
 
@@ -67,6 +195,65 @@ public class App {
         City Atlanta = new City("Atlanta", 486213, GA, true);
         GA.addCity(Atlanta);
         addState(stateList, GA);
+
+        listStates(stateList);
+
+        deleteState(stateList, CO);
+
+        System.out.println("----------");
+        listStates(stateList);
+
+        City SantaClarita = new City("Santa Clarita", 213411, CA, false);
+        City Sacramento = new City("Sacramento", 500930, CA, true);
+        addCityToState(CA, SantaClarita);
+        addCityToState(CA, Sacramento);
+
+        System.out.println("----------");
+        System.out.println("Cities in California:");
+        listCities(CA);
+
+        deleteCityFromState(CA, "Santa Clarita");
+        System.out.println("----------");
+        System.out.println("Cities in California:");
+        listCities(CA);
+
+        System.out.println("----------");
+        State stateName = searchStateByName(stateList, "New York");
+
+        System.out.println("----------");
+        State fakeName = searchStateByName(stateList, "Texas");
+
+        System.out.println("----------");
+        List<State> statesLessThan = searchLessThanPop(stateList, 1000000);
+        listStates(statesLessThan);
+
+        System.out.println("----------");
+        List<State> statesMoreThan = searchMoreThanPop(stateList, 1000000);
+        listStates(statesMoreThan);
+
+        System.out.println("----------");
+        City c = searchCityInState(CA, "Los Angeles");
+
+        System.out.println("----------");
+        City fakeCity = searchCityInState(CA, "Kablamville");
+
+        System.out.println("----------");
+        City capital = getCapital(CA);
+
+        System.out.println("----------");
+        City capital2 = getCapital(NY);
+
+        System.out.println("----------");
+        List<City> citiesLessThan = searchLessThanPopForCity(CA, 4500000);
+        for (City citi: citiesLessThan) {
+            System.out.println(citi.getName());
+        }
+
+        System.out.println("----------");
+        List<City> citiesMoreThan = searchMoreThanPopForCity(CA, 4500000);
+        for (City citi: citiesMoreThan) {
+            System.out.println(citi.getName());
+        }
 
     }
 
